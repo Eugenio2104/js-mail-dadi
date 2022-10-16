9b062730bf9ff967ae77f3db168e8a860b81933b
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-16 12:02:16 +0200

    esercizio dadi

--- js/script.js
+++ js/script.js
@@ -21,9 +21,9 @@ const mail =[
 ]
 let okMail = false;
 let index;
-const output = document.querySelector(`output`)
+const output = document.querySelector(`output`);
 
-const insertMail = prompt(`inserisci la tua mail`)
+const insertMail = prompt(`inserisci la tua mail`);
 console.log(mail);
 
 
@@ -40,3 +40,48 @@ if (okMail){
 }else{
   output.innerHTML = "Mail non valida";
 }
+
+
+
+/***Gioco dei dadi**
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+
+const minDado = 1;
+const maxDado = 6
+const numBtn = document.getElementById('lanciodadi');
+const container = document.querySelector('.risultato');
+
+let outputPerson;
+let outputCpu;
+let outputRisultato;
+let numPerson = 0;
+let numCpu = 0;
+
+numBtn.addEventListener('click', function(){
+// costanti
+container.innerHTML = '';
+numPerson = Math.floor(Math.random() * (maxDado - minDado + 1) + minDado);
+numCpu = Math.floor(Math.random() * (maxDado - minDado + 1) + minDado);
+
+// creazione div
+outputPerson = document.createElement('div');
+outputCpu = document.createElement('div');
+outputPerson.innerHTML = `Il tuo numero é:  ${numPerson}`;
+outputCpu.innerHTML = `Il numero del computer è:  ${numCpu}`;
+container.append(outputPerson);
+container.append(outputCpu);
+
+//condizioni
+outputRisultato = document.createElement('div');
+if(numPerson > numCpu){
+  outputRisultato = 'HAI VINTO, SEI IL RE DEI DADI!';
+}else if(numCpu > numPerson ){
+  outputRisultato = 'LOOSER';
+}else{
+  outputRisultato = 'PAREGGIO!';
+}
+
+container.append(outputRisultato);
+})
